542cccba87f5e526f6ca83e7323741d8c67ab31c
Edwin Lucke <edwin.lucke@live.com>
2018-08-14 00:31:14 +0200

    setup all core porjects and push to develop branch on github

---
+++ qtree.core.consoleapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qtree.core.consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ qtree.core.mstest.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace qtree.core.mstest.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
